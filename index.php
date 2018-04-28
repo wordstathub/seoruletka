c1b15779989eef9c8a03872dea84337daeb090ad
wordstathub <38808269+wordstathub@users.noreply.github.com>
2018-04-28 12:35:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,22 @@
 <br/>
 <br/>
 <br/>
-<big>text</big>
+<big>
+  <script>
+"use strict";
+window.onload = function(){
+let animals = [
+{num:1,name:'волк'}, 
+{num:2, name: 'лиса'}
+{num:3, name: 'test'}
+];
+let obj = animals[getRandomInt(0,2)];
+let output = obj.num +' '+ obj.name;
+alert(output); 
+};
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+</script>
+  </big>
 </center>
