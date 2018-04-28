26778a16c0d15ca468689727cbde4309e84aa5cb
wordstathub <38808269+wordstathub@users.noreply.github.com>
2018-04-28 12:46:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
   <h2>Кому достанется проект?</h2>
 <big>
 <?php
-  $quotes[] = 'Саша';
-    $quotes[] = 'Илья';
-    $quotes[] = 'Николай';
- srand ((double) microtime() * 1000000);
-    $random_number = rand(0,count($quotes)-1);
- echo ($quotes[$random_number]);  
+$quotes[] = 'Саша';
+$quotes[] = 'Илья';
+$quotes[] = 'Николай';
+srand ((double) microtime() * 1000000);
+$random_number = rand(0,count($quotes)-1);
+echo ($quotes[$random_number]);  
 ?>
-  </big>
+</big>
 </center>
